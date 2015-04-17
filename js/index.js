f88002d503932d38c0ada5f26e650fd026be2ac0
dbwyatt <dbwyatt23@gmail.com>
2015-04-16 23:42:38 -0600

    Finally got less working appropriately with SimpLESS. @import causes issues when not in quotes. Fixed the functionality of page changes with hash change. Moved navbar back to top of page.

--- js/index.js
+++ js/index.js
@@ -5,23 +5,60 @@ $(function() {
 	// 	$(this).addClass("active");
 	// }); // navbar link click
 
-	$('[data-target="page"]').on('click', function() {
-		var $this = $(this);
-		var current_page = "#" + $('.current-page').attr('id');
-		if ($this.attr('data-page') != current_page) {
-			$(window).trigger("page_load");
+	if (window.location.hash != '') {
+		pageChange(window.location.hash);
+	}
+	else {
+		pageChange('#home');
+	}
+	
+	$(window).on('hashchange', function() {
+		if (window.location.hash == '') {
+			pageChange('#home-page');
 		}
-	}); // page change
+		else {
+			pageChange(window.location.hash);
+		}
+	}); //hashchange
 
+	// $('[data-target="page"]').on('click', function() {
+	// 	var $this = $(this);
+	// 	var current_page = "#" + $('.current-page').attr('id');
+	// 	if ($this.attr('data-page') != current_page) {
+	// 		// $(window).trigger("page_load");
+	// 		pageChange($this.data('page'));
+	// 	}
+	// }); // page change
 
-	$(window).off("page_load").on("page_load", function() {
-		var hash = window.location.hash;
-		console.log(hash);
+	function pageChange(page) {
+		console.log(page);
 		$('.current-page').removeClass('current-page').fadeOut("fast", function() {
-			$($(hash).attr('data-page')).fadeIn().addClass('current-page');
+			$(page).fadeIn().addClass('current-page');
 		});
-	}); //page_load function
+	}
 
-	$(window).trigger("page_load");
+	// $(window).off("page_load").on("page_load", function(event) {
+	// 	var hash = window.location.hash;
+	// 	console.log(hash);
+	// 	$('.current-page').removeClass('current-page').fadeOut("fast", function() {
+	// 		$($(hash).attr('data-page')).fadeIn().addClass('current-page');
+	// 	});
+	// }); //page_load function
+
+	// $(window).trigger("page_load");
+
+	// $('a[href*=#]:not([href=#])').click(function() {
+	// 	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+
+	// 		var target = $(this.hash);
+	// 		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	// 		if (target.length) {
+	// 			$('html,body').animate({
+	// 			   scrollTop: target.offset().top
+	// 			}, 1000);
+	// 			return false;
+	// 		}
+	// 	}
+	// });
 
 }); //ready
